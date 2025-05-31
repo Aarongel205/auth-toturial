43a5ec4dac876869a046e2f751f0f7d23578b86f
Aarongel205 <aaronadanza92@gmail.com>
2025-05-31 15:42:58 +0800

    added padding-5

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
     <div id='wrapper' className='bg-[url(src/assets/nightForest.jpg)] h-screen flex flex-col bg-cover bg-center justify-center items-center'>
-      <div id='container' className='bg-white/5 w-100 h-100 backdrop-blur-md rounded-md boder-1 border-gray-600 flex flex-col'>
+      <div id='container' className='bg-white/5 w-100 h-100 backdrop-blur-md rounded-md boder-1 border-gray-600 flex flex-col p-5'>
         <h4 className="text-white text-center text-2xl font-bold">Login</h4>
       </div>
     </div>
