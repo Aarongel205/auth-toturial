1904c95e7d0eeaa57aac0c34e8f6c8440dbbd595
Aarongel205 <aaronadanza92@gmail.com>
2025-05-31 15:14:52 +0800

    added bg-image

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 function App() {
   return (
     <>
-    
+    <div id='wrapper' className='bg-[url(src/assets/nightForest.jpg)] h-screen flex flex-col bg-cover bg-center justify-center align-center'>
+    </div>
     </>
   )
 }
