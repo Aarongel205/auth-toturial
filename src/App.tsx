f726984f998408744467a37fa8efe9cca9e017b1
Aarongel205 <aaronadanza92@gmail.com>
2025-05-31 15:21:45 +0800

    added centered container

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 function App() {
   return (
     <>
-    <div id='wrapper' className='bg-[url(src/assets/nightForest.jpg)] h-screen flex flex-col bg-cover bg-center justify-center align-center'>
+    <div id='wrapper' className='bg-[url(src/assets/nightForest.jpg)] h-screen flex flex-col bg-cover bg-center justify-center items-center'>
+      <div id='container' className='bg-red-600 w-100 h-100'></div>
     </div>
     </>
   )
